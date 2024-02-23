d7abc67bb042479db9e19ee4139e0547f1c4f306
nle75234 <nle75234@gmail.com>
2024-02-23 10:38:34 +0700

    First Commit

---
+++ 3DModelPreview.xaml.cs
@@ -0,0 +1,43 @@
+﻿
+using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace Convert3DObject
+{
+    /// <summary>
+    /// Interaction logic for _3DModelPreview.xaml
+    /// </summary>
+    public partial class _3DModelPreview : Window
+    {
+        public Model3D _model;
+        
+        public _3DModelPreview(Model3D model)
+        {
+            InitializeComponent();
+            _model = model;
+            DisplayModel();
+        }
+
+        public void DisplayModel()
+        {
+            ModelVisual3D modelVisual = new ModelVisual3D();
+            modelVisual.Content = _model;
+
+            viewPort3d.Children.Add(modelVisual);
+            viewPort3d.Camera.UpDirection = new Vector3D(0, 1, 0);
+        }
+    }
+}
---
+++ MainMenu.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Assimp;
+using HelixToolkit.Wpf;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace Convert3DObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        string? filePath;
+        string? fileExtension;
+
+        public Model3D model = null;
+        public MainMenu()
+        {
+            InitializeComponent();
+            FileURL.IsReadOnly = true;
+            ExtensionList.Items.Add("STL");
+            ExtensionList.Items.Add("OBJ");
+            ExtensionList.Items.Add("FBX");
+            ExtensionList.Items.Add("GLTF");
+            ExtensionList.Items.Add("GLB");
+
+        }
+        private void Browser_Click(object sender, RoutedEventArgs e) //Finish
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileURL.Text = dialog.FileName;
+                filePath = dialog.FileName;
+            }
+        }
+        private void Convert_Click(object sender, RoutedEventArgs e) // Finish
+        {
+            Validation(fileExtension, filePath);
+            Convert3DFile(fileExtension, filePath);
+        }
+        private void ExtensionList_SelectionChanged(object sender, SelectionChangedEventArgs e) // Finish
+        {
+            fileExtension = ExtensionList.SelectedItem.ToString();
+
+        }
+              private void Convert3DFile(string fileExtention, string filepath) //Finish
+        {
+            string outputPath = Path.GetDirectoryName(filePath) + "\\" + Path.GetFileNameWithoutExtension(filePath) + "." + fileExtension.ToLower();
+            var scene = new AssimpContext();
+            var model = scene.ImportFile(filepath);
+  
+            scene.ExportFile(model, outputPath, fileExtention.ToLower());
+        }
+        public void Validation(string fileExtenstion, string filePath)
+        {
+            if (filePath == null || filePath.Equals(""))
+            {
+                System.Windows.MessageBox.Show("Invalid File Path");
+                return;
+            }
+            if (fileExtension == null || fileExtension.Equals(""))
+            {
+                System.Windows.MessageBox.Show("Please choose output file extension");
+                return;
+            }
+        }
+        public void LoadModel(string path)
+        {        
+            if (filePath == null || filePath.Equals(""))
+            {
+                System.Windows.MessageBox.Show("Invalid File Path");
+                return;
+            }
+          
+            if (Path.GetExtension(path).ToLower().Equals(".obj") || Path.GetExtension(path).ToLower().Equals(".stl"))
+            {
+                ModelImporter import = new ModelImporter();
+                model = import.Load(path);
+                _3DModelPreview preview = new _3DModelPreview(model);
+                preview.Show();
+                return;
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Only support preview OBJ and STL file");
+                return;
+            }
+        }
+        private void Preview_Click(object sender, RoutedEventArgs e)
+        {
+            LoadModel(filePath);    
+        }
+    }
+}
